5829e2e25d0aed58f10921efd79b33b560d08a36
Aldar Batyrev <aldaror19@gmail.com>
2020-03-22 21:33:17 +0300

    feat: add solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,63 @@
 class VigenereCipheringMachine {
+    constructor(value = true) {
+        this.direct = value;
+    }
     encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+        if ( !message || !key ) {
+            throw new Error("error");
+        }
+
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+        
+        const offset = "A".charCodeAt(0);
+        const alphabetSize = 26;
+
+        result = "";
+        keyIndex = 0;
+        for (i = 0; i < message.length; i++) {
+            if ( message.charCodeAt(i) < offset || message.charCodeAt(i) > offset + alphabetSize ) {
+                result += message[i];
+                continue;
+            } else {
+                index = ((message.charCodeAt(i) - offset + key.charCodeAt(keyIndex) - offset) % alphabetSize) + offset;
+                result += String.fromCharCode(index);
+            }
+            keyIndex++;
+            if(keyIndex == key.length){
+                keyIndex = 0;
+            }
+           
+        }
+        return this.direct ? result : result.split('').reverse().join('');
     }
 
     decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+        if ( !message || !key ) {
+            throw new Error("error");
+        }
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+        const offset = "A".charCodeAt(0);
+        const alphabetSize = 26;
+
+        result = "";
+        keyIndex = 0;
+
+        for (i = 0; i < message.length; i++) { 
+            if ( message.charCodeAt(i) < offset || message.charCodeAt(i) > offset + alphabetSize ) {
+                result += message[i];
+                continue;
+            } else {
+                index = (((message.charCodeAt(i) - offset) + alphabetSize - (key.charCodeAt(keyIndex) - offset)) % alphabetSize) + offset
+                result += String.fromCharCode(index);
+            }
+            keyIndex++;
+            if(keyIndex == key.length){
+                keyIndex = 0;
+            }
+        }
+        return this.direct ? result : result.split('').reverse().join('');
     }
 }
 
